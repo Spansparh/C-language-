358573f9306018e73d2d03f7302467a73b47cf05
Spansparh <spansparh@gmail.com>
2024-11-16 13:51:32 +0300

    Add files via upload

---
+++ 1Darray.c
@@ -0,0 +1,19 @@
+//Arrays in c language
+/*
+author:Kelvin
+Reg no:CT101/G/25008/24
+date:09/11/24
+*/
+//preprocessor directves
+#include<stdio.h>
+
+int main(){
+int i;
+int marks[5]={50,60,72,67,81};
+for(i=0;i<5;i++){
+printf("marks[%d]=%d\n",i,marks[i]);
+}
+
+return 0;
+
+}
