a64bd3b05df42585b387f949847d9420957c60f2
Spansparh <spansparh@gmail.com>
2024-11-16 14:33:25 +0300

    Add files via upload

---
+++ functions.c
@@ -0,0 +1,40 @@
+//Functions in c language
+/*
+author:Kelvin
+Reg no:CT101/G/25008/24
+date:09/11/24
+*/
+//preprocessor directves
+#include<stdio.h>
+#include<math.h>
+int sum(int x,int y,int z);//Function prototype
+int multiplication(int a,int b,int c);//Function prototype
+
+int main(){
+    int a,b,c,x,y,z;
+    printf("\nEnter 3 values:\n");
+    scanf("\n%d%d%d",&a,&b,&c);
+
+    //Function calling
+    x=sum(a,b,c);
+    y=sum(100,200,300);
+    z=multiplication(a,b,c);
+
+    printf("\nthe sum is:%d",x);
+    printf("\nthe 2nd sum is:%d",y);
+    printf("\nthe multiplication is:%d",z);
+
+    return 0;
+}
+//function definition
+int sum(int x,int y,int z){
+    int results;
+    results=x+y+z;
+    return results;
+}
+//function definition
+int multiplication(int a,int b,int c){
+    int results;
+    results=a*b*c;
+    return results;
+}
